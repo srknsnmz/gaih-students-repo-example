34c655f2ef996d708f6a230b1c42e9e88609dfaf
srknsnmz <45612709+srknsnmz@users.noreply.github.com>
2021-02-06 15:00:22 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,17 @@
-#Explain your work
+#QUESTION1
 
-#Question 1
-for x in range(a):
-	print(a)
+user_name = "serkan"
+password ="123456"
+
+user_name1 =input("Please enter your user name: ")
+
+password1= input("Please enter your password: ")
+
+if (user_name != user_name1 and password == password1):
+    print("Invalid user name")
+elif (user_name==user_name1 and password != password1):
+    print("Invalid Password")
+elif (user_name != user_name1 and password!= password1):
+    print("Invalid user name and password")
+else:
+    print("You are now logged in...")
