af0c8a15617521a243dcd85f3d949ff56bd60326
srknsnmz <45612709+srknsnmz@users.noreply.github.com>
2021-02-06 14:52:40 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,12 +1,33 @@
+#QUESTION1
+
+NumList = []
+ 
+Number = int(input("How many elements in list :- "))
+ 
+
+while ( Number%2 != 0 ):
+   print("This program will not accept odd number.")
+   Number = int(input("How many elements in list :- "))
+   break
+ 
+for i in range(1, Number + 1):
+    value = int(input("Please enter the Value of %d Element :- " %i))
+    NumList.append(value)
+ 
+num = int(Number/2)
+ 
+list1 = NumList[:num]
+
+list2 = NumList[num:]
+ 
+swapList = list2 + list1
 
 
-
-
-
-
-
-
-
+print(num)
+print("list : ",NumList)
+print("list1: ",list1)
+print("list2: ",list2)
+print(swapList)
 
 
 #QUESTION2
