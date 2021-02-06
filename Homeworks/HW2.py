83fa6b0bbefc885f2dd606b7361ec6db46592d0b
srknsnmz <45612709+srknsnmz@users.noreply.github.com>
2021-02-06 15:19:07 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -15,3 +15,17 @@ elif (user_name != user_name1 and password!= password1):
     print("Invalid user name and password")
 else:
     print("You are now logged in...")
+    
+    
+    
+    #USING DICTIONARY
+    
+    database ={'serkan': '1234', 'ali': '5678', 'ayse': '9012'}
+username = input('Enter username: ')
+password = input('Enter password: ')
+if username not in database:
+      print("The user does not exist")
+elif database[username] == password:
+      print("Congrats! You're logged in")
+elif database[username] != password:
+      print("Sorry - wrong combination")
