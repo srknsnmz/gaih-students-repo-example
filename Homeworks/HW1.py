55445f162ec6456129216985be7b112b956452b4
srknsnmz <45612709+srknsnmz@users.noreply.github.com>
2021-02-06 14:51:19 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,43 @@
-#Explain your work
 
-#Question 1
-for x in range(a):
-	print(a)
+
+
+
+
+
+
+
+
+
+
+#QUESTION2
+ 
+Number = int(input("Please enter a single digit integer: "))
+ 
+
+while ( Number < 0 or 10 <= Number ):
+   print("please enter a single digit integer between 0 and 9 ")
+   Number = int(input("Please enter a single digit integer: "))
+   exit()
+
+MyList = list(range(Number + 1))
+print(MyList)
+ 
+
+evenList = []
+
+for i in range(Number+1):
+  if MyList[i] % 2  == 0:
+    evenList.append(MyList[i])
+  exit()
+
+print(evenList)
+
+
+  
+
+
+
+
+
+
+
