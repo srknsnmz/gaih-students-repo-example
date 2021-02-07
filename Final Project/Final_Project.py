3effa1043165824523e7305e864a1e3d3e186e60
srknsnmz <45612709+srknsnmz@users.noreply.github.com>
2021-02-07 16:13:41 +0300

    Update Final_Project.py

--- Final Project/Final_Project.py
+++ Final Project/Final_Project.py
@@ -1,3 +1,41 @@
-import pandas as pd
+class Employee():
+    
+    def __init__(self,name, age): 
+        self.name = name
+        self.age = age
+        self.language = []
 
-data = pd.read_csv("project.csv")
+    def addLanguage(self,new_lang):
+        self.language.append(new_lang)
+    
+
+    def showInfo(self):
+        print(self.name + " is " + self.age + " years old")
+        for i in self.language: 
+          print("language spoken: " + i)
+
+class manager(Employee):
+
+   
+    def __init__(self,name, age):
+        super().__init__(name, age)
+       
+       
+    def showInfo(self):
+        super().showInfo()
+        
+    
+ahmet = Employee("ahmet","23")
+mehmet = Employee("mehmet","29")
+ayse = Employee("Ayse","35")
+zeynep = manager("Zeynep","35")
+ahmet.addLanguage("Turkish"),ahmet.addLanguage("English"),
+mehmet.addLanguage("English"),mehmet.addLanguage("Turkish")
+ayse.addLanguage("English"),ayse.addLanguage("Turkish")
+zeynep.addLanguage("Turkish"),zeynep.addLanguage("English"),zeynep.addLanguage("French"),zeynep.addLanguage("Spanish")
+
+
+ahmet.showInfo()
+mehmet.showInfo()
+ayse.showInfo()
+zeynep.showInfo()
